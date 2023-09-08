35d03e91307eaf4d3ab6428b0a35209835a7b088
ctrlbugs <azee@techie.com>
2023-09-08 06:08:24 +0100

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Hajia Bintu</h1>
+        <h5>Hi There 👋</h5>
+        <h1>I am Aze</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocial />
